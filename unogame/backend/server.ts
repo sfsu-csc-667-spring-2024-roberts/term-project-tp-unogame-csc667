d97030427d3624a95b47a8f8dc2f0d9b06a8750c
Y-Y1Q <sh4bee@outlook.com>
2024-03-24 20:35:25 -0700

    chore: Added dev env setup

--- unogame/backend/server.ts
+++ unogame/backend/server.ts
@@ -1,13 +1,26 @@
 import path from "path";
 import express from "express";
 import createError from "http-errors";
-import morgan from "morgan";
 import rootRoutes from "./routes/root";
 import testRoutes from "./routes/test";
+import { setUpDevelopmentEnvironment } from "./utilities/set-up-development-environment";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
+
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+
+  setUpDevelopmentEnvironment();
+
+  app.use(require('connect-livereload')({
+    port: 35729
+  }));
+  
+}
+
+const morgan = require("morgan");
 
 app.use(morgan("dev"));
 
